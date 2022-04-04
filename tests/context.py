f6251ed1b93e42be7704ae0db18f5194896918d2
Jacob Brunson <github@jacobbrunson.com>
2022-04-04 17:47:37 +0000

    Added a unit test

---
+++ tests/context.py
@@ -0,0 +1,10 @@
+"""
+This module helps us test without having to install the package"""
+
+import os
+import sys
+
+addpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, addpath)
+
+import stingeripc
