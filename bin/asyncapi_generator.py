f21d220e2c1d5b163af7b49f4f8821ae11ece96e
Jacob Brunson <github@jacobbrunson.com>
2022-04-25 04:14:20 +0000

    Redo asyncapi WIP

--- bin/asyncapi_generator.py
+++ bin/asyncapi_generator.py
@@ -1,27 +1,22 @@
-import yaml
-import yamlloader
-from jacobsjinjatoo import templator as jj2
+import os
+import sys
 
 from stingeripc.asyncapi import AsyncApiCreator
+from stingeripc import StingerInterface
+
+libpath = os.path.normpath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
+)
+sys.path.append(libpath)
 
 if __name__ == '__main__':
     inname = sys.argv[1]
     outdir = sys.argv[2]
-    outname_template = "%s-{}.asyncapi.yaml" % (os.path.splitext(os.path.basename(inname))[0])
-    outname_server = os.path.join(outdir, outname_template.format("server"))
-    outname_client = os.path.join(outdir, outname_template.format("client"))
-    outname_common = os.path.join(outdir, outname_template.format("common"))
-    idef = yaml.load(open(inname), Loader=yaml.Loader)
-    converter = AsyncApiCreator()
-    converter.add_stinger(idef)
-    a2s_server = converter.get_asyncapi(SpecType.SERVER)
-    yaml.dump(a2s_server, open(outname_server, 'w'), Dumper=yamlloader.ordereddict.CDumper)
-    a2s_client = converter.get_asyncapi(SpecType.CLIENT)
-    yaml.dump(a2s_client, open(outname_client, 'w'), Dumper=yamlloader.ordereddict.CDumper)
+
+    with open(inname, "r") as f:
+        stinger = StingerInterface.from_yaml(f)
     params = {
-        "a2s": converter,
-        "stinger": idef,
+        "stinger": stinger,
     }
-    t = jj2.CodeTemplator(output_dir=os.path.dirname(sys.argv[2]))
-    t.add_template_dir(os.path.join(os.path.dirname(__file__), "templates", "python"))
-    t.render_template("server.py.jinja2", "server.py", **params)
+
+    converter = AsyncApiCreator()
