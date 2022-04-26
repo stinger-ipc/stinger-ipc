7db05e866207118e8017892ff46fba8256e8cfbe
Jacob Brunson <github@jacobbrunson.com>
2022-04-26 20:15:47 +0000

    Add rust enums

---
+++ example_interfaces/full/output/rust/src/enums.rs
@@ -0,0 +1,16 @@
+/*
+DO NOT MODIFY THIS FILE.  It is automatically generated and changes will be over-written
+on the next generation.
+
+It contains enumerations used by the Example interface.
+*/
+
+
+enum DayOfTheWeek {
+    Sunday = 1,
+    Monday = 2,
+    Tuesday = 3,
+    Wednesday = 4,
+    Thursday = 5,
+    Friday = 6
+}
