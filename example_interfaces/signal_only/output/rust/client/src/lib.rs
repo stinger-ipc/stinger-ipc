99a2ea805f3988495252f5c2035b40cd75c0144e
Jacob Brunson <github@jacobbrunson.com>
2022-11-14 04:03:40 +0000

    Rust client with method compiles. Not yet tested.

--- example_interfaces/signal_only/output/rust/client/src/lib.rs
+++ example_interfaces/signal_only/output/rust/client/src/lib.rs
@@ -5,21 +5,33 @@ on the next generation.
 This is the Client for the SignalOnly interface.
 */
 
-use futures::StreamExt;
+
+use futures::{StreamExt};
 use connection::Connection;
+
+use paho_mqtt::topic_matcher::TopicMatcher;
 
 pub struct SignalOnlyClient {
     connection: Connection,
     signal_recv_callback_for_another_signal: Box<dyn FnMut(f32, bool, String)->()>,
+    
+    topic_matcher: TopicMatcher::<u32>,
     
 }
 
 impl SignalOnlyClient {
     pub fn new(connection: Connection) -> SignalOnlyClient {
 
+        let mut topic_matcher = TopicMatcher::<u32>::new();
+        topic_matcher.insert("SignalOnly/signal/anotherSignal", 1235);
+        
+        
+
         SignalOnlyClient {
             connection: connection,
             signal_recv_callback_for_another_signal: Box::new( |_1, _2, _3| {} ),
+            
+            topic_matcher: topic_matcher,
             
         }
     }
@@ -30,24 +42,41 @@ impl SignalOnlyClient {
     }
     
 
+    
+
     pub async fn process(&mut self) {
         while let Some(opt_msg) = self.connection.rx.next().await {
             if let Some(msg) = opt_msg {
-                let payload_object = json::parse(&msg.payload_str()).unwrap();
-                if msg.topic() == "SignalOnly/signal/anotherSignal" {
-                    
-                    let temp_one = payload_object["one"].as_f32().unwrap();
-                    
-                    
-                    let temp_two = payload_object["two"].as_bool().unwrap();
-                    
-                    
-                    let temp_three = payload_object["three"].as_str().unwrap().to_string();
-                    
-                    
-                    (self.signal_recv_callback_for_another_signal)(temp_one, temp_two, temp_three);
+                let topic = &msg.topic();
+                let mut func_indexs: Vec<u32> = Vec::new();
+                for item in self.topic_matcher.matches(topic) {
+                    func_indexs.push(item.1);
                 }
-                
+                for func_index in func_indexs.iter() {
+                    if func_index >= &1234 {
+                        let payload_object = json::parse(&msg.payload_str()).unwrap();
+                        match func_index {
+                            1235 => {
+                                
+                                let temp_one = payload_object["one"].as_f32().unwrap();
+                                
+                                
+                                let temp_two = payload_object["two"].as_bool().unwrap();
+                                
+                                
+                                let temp_three = payload_object["three"].as_str().unwrap().to_string();
+                                
+                                
+                                (self.signal_recv_callback_for_another_signal)(temp_one, temp_two, temp_three);
+                            }
+                            _ => ()
+                        }
+                    
+                    } else {
+                        match func_index {_ => ()
+                        }
+                    }
+                }
             }
         }
     }
