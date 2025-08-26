1ef60fe40a0295121c1d822aee940d6f47b5a561
Jacob Brunson <jacob.brunson@vivint.com>
2025-08-26 17:41:10 -0600

    updated tools

--- stingeripc/tools/markdown_generator.py
+++ stingeripc/tools/markdown_generator.py
@@ -1,12 +1,15 @@
 import os
 import sys
 from jacobsjinjatoo import templator as jj2
-
+import os.path
+import typer
+from typing_extensions import Annotated
 from stingeripc import StingerInterface
+from rich import print
+from pathlib import Path
 
-def main():
-    inname = sys.argv[1]
-    outdir = sys.argv[2]
+def main(inname: Annotated[Path, typer.Argument(exists=True, file_okay=True, dir_okay=False, readable=True)], outdir: Annotated[Path, typer.Argument(file_okay=False, dir_okay=True, writable=True, readable=True)]):
+    print(f"[bold green]Reading:[/bold green] {inname}")
     with open(inname, "r") as f:
         stinger = StingerInterface.from_yaml(f)
     params = {
@@ -19,7 +22,11 @@ def main():
     for output_file in [
         "index.md",
     ]:
+        print(f"[bold red]GENERATING:[/bold red] {output_file}")
         t.render_template(f"{output_file}.jinja2", output_file, **params)
 
-if __name__ == '__main__':
-    main()
+def run():
+    typer.run(main)
+
+if __name__ == "__main__":
+    run()
--- stingeripc/tools/python_generator.py
+++ stingeripc/tools/python_generator.py
@@ -1,13 +1,15 @@
 from jacobsjinjatoo import templator as jj2
 from jacobsjinjatoo import stringmanip
-import sys
 import os.path
-
+import typer
+from typing_extensions import Annotated
 from stingeripc import StingerInterface
+from rich import print
+from pathlib import Path
 
-def main():
-    inname = sys.argv[1]
-    outdir = sys.argv[2]
+def main(inname: Annotated[Path, typer.Argument(exists=True, file_okay=True, dir_okay=False, readable=True)], outdir: Annotated[Path, typer.Argument(file_okay=False, dir_okay=True, writable=True, readable=True)]):
+
+    print(f"[bold green]Reading:[/bold green] {inname}")
     with open(inname, "r") as f:
         stinger = StingerInterface.from_yaml(f)
     params = {
@@ -20,10 +22,13 @@ def main():
     for output_file in [
         "pyproject.toml",
     ]:
-        t.render_template(f"{output_file}.jinja2", output_file, **params)
+        print(f"[bold red]Generating:[/bold red] {output_file}")
+        t.render_template(f"{output_file}.jinja2", os.path.join(outdir, output_file), **params)
 
-    generated_src_dir = os.path.join(outdir, f"{stringmanip.lower_camel_case(stinger.name).lower()}ipc")
-    os.makedirs(generated_src_dir, exist_ok=True)
+    package_name = f"{stringmanip.lower_camel_case(stinger.name).lower()}ipc"
+    generated_pkg_dir = os.path.join(outdir, package_name)
+    print(f"[bold red]Making Directory:[/bold red] {generated_pkg_dir}")
+    os.makedirs(generated_pkg_dir, exist_ok=True)
     for output_file in [
         "server.py",
         "client.py",
@@ -31,9 +36,13 @@ def main():
         "__init__.py",
         "method_codes.py",
     ]:
-        t.render_template(f"{output_file}.jinja2", os.path.join(stinger.name.lower(), output_file), **params)
+        print(f"[bold red]Generating:[/bold red] {output_file}")
+        t.render_template(f"{output_file}.jinja2", os.path.join(generated_pkg_dir, output_file), **params)
 
-    t.render_template("interface_types.py.jinja2", os.path.join(stinger.name.lower(), f"{stinger.get_enum_module_name()}.py"), **params)
+    t.render_template("interface_types.py.jinja2", os.path.join(generated_pkg_dir, f"{stinger.get_enum_module_name()}.py"), **params)
+
+def run():
+    typer.run(main)
 
 if __name__ == "__main__":
-    main()
+    run()
