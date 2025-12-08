cfed3581187b53893324caee4d414961389a0f9a
Jacob Brunson <jacob.brunson@vivint.com>
2025-12-08 09:39:38 -0700

    0.5.3

--- example_interfaces/testable/output/python/examples/client_demo_classes.py
+++ example_interfaces/testable/output/python/examples/client_demo_classes.py
@@ -349,7 +349,7 @@ class SuperAwesomeDoerOfThings:
                     optional_string="apples",
                     optional_enum=Numbers.ONE,
                     optional_entry_object=Entry(key=42, value="apples"),
-                    optional_date_time=datetime.now(UTC),
+                    optional_date_time=None,
                     optional_duration=None,
                     optional_binary=b"example binary data",
                     array_of_integers=[42, 2022],
@@ -390,7 +390,7 @@ class SuperAwesomeDoerOfThings:
                     optional_string="apples",
                     optional_enum=Numbers.ONE,
                     optional_entry_object=Entry(key=42, value="apples"),
-                    optional_date_time=datetime.now(UTC),
+                    optional_date_time=None,
                     optional_duration=None,
                     optional_binary=b"example binary data",
                     array_of_integers=[42, 2022],
@@ -529,7 +529,7 @@ class SuperAwesomeDoerOfThings:
             sleep(5)
 
             print("Making call to 'call_optional_date_time'")
-            future_resp = self.client.call_optional_date_time(input1=None)
+            future_resp = self.client.call_optional_date_time(input1=datetime.now(UTC))
             try:
                 print(f"RESULT:  {future_resp.result(5)}")
             except futures.TimeoutError:
