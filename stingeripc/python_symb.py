80a01ab264a61edeee5b1fe128d1d93811dbdfe3
Jacob Brunson <github@jacobbrunson.com>
2026-04-10 19:03:24 -0600

    Split apart language symbols

---
+++ stingeripc/python_symb.py
@@ -0,0 +1,312 @@
+from typing import Any
+
+from jacobsjinjatoo import stringmanip
+
+from stingeripc.args import ArgPrimitiveType
+from stingeripc.lang_symb import ISymbolsProvider
+
+
+class PythonSymbolsProvider(ISymbolsProvider):
+
+    def for_model(self, model_class_name: str, model) -> object | None:
+        if model_class_name == "StingerSpec":
+            return PythonInterfaceSymbols(model, self.config)
+        elif model_class_name == "InterfaceStruct":
+            return PythonStructSymbols(model, self.config)
+        elif model_class_name == "InterfaceEnum":
+            return PythonEnumSymbols(model)
+        elif model_class_name == "Method":
+            return PythonMethodSymbols(model, self.config)
+        elif model_class_name == "Property":
+            return PythonPropertySymbols(model, self.config)
+        elif model_class_name == "ArgEnum":
+            return PythonArgEnumSymbols(model)
+        elif model_class_name == "ArgPrimitive":
+            return PythonArgPrimitiveSymbols(model)
+        elif model_class_name == "ArgStruct":
+            return PythonArgStructSymbols(model)
+        elif model_class_name == "ArgDateTime":
+            return PythonArgDateTimeSymbols(model)
+        elif model_class_name == "ArgDuration":
+            return PythonArgDurationSymbols(model)
+        elif model_class_name == "ArgBinary":
+            return PythonArgBinarySymbols(model)
+        elif model_class_name == "ArgArray":
+            return PythonArgArraySymbols(model)
+        return None
+
+
+class PythonSymbols:
+
+    def __init__(self, config: dict[str, Any] | None = None):
+        self.config = config
+
+    @property
+    def type_definition_module(self) -> str:
+        return "interface_types"
+
+
+class PythonInterfaceSymbols(PythonSymbols):
+
+    def __init__(self, interface, config: dict[str, Any] | None = None):
+        super().__init__(config)
+        self._iface = interface
+
+    @property
+    def package_directory(self) -> str:
+        s = f"{stringmanip.lower_only(self._iface.name).lower()}{stringmanip.lower_only(self.config.python.package_suffix) or 'ipc'}"
+        return s
+
+    @property
+    def package_name(self):
+        s = f"{stringmanip.hyphen_case(self._iface.name).lower()}-{stringmanip.hyphen_case(self.config.python.package_suffix) or 'ipc'}"
+        return s
+
+    @property
+    def module_name(self) -> str:
+        return self.package_directory
+
+    @property
+    def client_class_name(self) -> str:
+        """ Name of the python class for the interface client."""
+        return f"{stringmanip.upper_camel_case(self._iface.name)}Client"
+
+    @property
+    def server_class_name(self) -> str:
+        """ Name of the python class for the interface server."""
+        return f"{stringmanip.upper_camel_case(self._iface.name)}Server"
+
+
+class PythonStructSymbols(PythonSymbols):
+
+    def __init__(self, iface_struct, config: dict[str, Any] | None = None):
+        super().__init__(config)
+        self._iface_struct = iface_struct
+
+    @property
+    def type(self) -> str:
+        return stringmanip.upper_camel_case(self._iface_struct.name)
+
+    @property
+    def local_type(self) -> str:
+        return stringmanip.upper_camel_case(self._iface_struct.name)
+
+
+class PythonEnumSymbols(PythonSymbols):
+
+    def __init__(self, enum):
+        super().__init__()
+        self._enum = enum
+
+    @property
+    def type(self) -> str:
+        return stringmanip.upper_camel_case(self._enum.name)
+
+    @property
+    def local_type(self) -> str:
+        return stringmanip.upper_camel_case(self._enum.name)
+
+
+class PythonArgSymbols(PythonSymbols):
+    """Base Python symbols for Arg objects."""
+
+    def __init__(self, arg):
+        super().__init__()
+        self._arg = arg
+
+    @property
+    def type(self) -> str:
+        return self._arg.name
+
+    @property
+    def class_name(self) -> str:
+        return self.type
+
+    @property
+    def local_type(self) -> str:
+        return self.type.split(".")[-1]
+
+    @property
+    def annotation(self) -> str:
+        return self.class_name
+
+
+class PythonArgEnumSymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return self._arg.enum.python.type
+
+    @property
+    def local_type(self) -> str:
+        return self._arg.enum.python.local_type
+
+    @property
+    def class_name(self) -> str:
+        return self._arg.enum.python.type
+
+    @property
+    def annotation(self) -> str:
+        if self._arg.optional:
+            return f"Optional[{self._arg.enum.python.type}]"
+        return self._arg.enum.python.type
+
+
+class PythonArgPrimitiveSymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return ArgPrimitiveType.to_python_type(self._arg.primitive_type)
+
+    @property
+    def annotation(self) -> str:
+        return ArgPrimitiveType.to_python_type(self._arg.primitive_type, optional=self._arg.optional)
+
+
+class PythonArgStructSymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return self._arg.interface_struct.python.local_type
+
+    @property
+    def local_type(self) -> str:
+        return self._arg.interface_struct.python.local_type
+
+    @property
+    def annotation(self) -> str:
+        if self._arg.optional:
+            return f"Optional[{self.type}]"
+        return self.type
+
+
+class PythonArgDateTimeSymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return "datetime"
+
+    @property
+    def local_type(self) -> str:
+        return "datetime"
+
+    @property
+    def annotation(self) -> str:
+        if self._arg.optional:
+            return "Optional[datetime]"
+        return "datetime"
+
+
+class PythonArgDurationSymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return "timedelta"
+
+    @property
+    def annotation(self) -> str:
+        if self._arg.optional:
+            return "Optional[timedelta]"
+        return "timedelta"
+
+
+class PythonArgBinarySymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return "bytes"
+
+    @property
+    def annotation(self) -> str:
+        if self._arg.optional:
+            return f"Optional[{self.type}]"
+        return self.type
+
+
+class PythonArgArraySymbols(PythonArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return "list"
+
+    @property
+    def annotation(self) -> str:
+        if self._arg.optional:
+            return f"Optional[List[{self._arg.element.python.annotation}]]"
+        return f"List[{self._arg.element.python.annotation}]"
+
+
+class PythonMethodSymbols(PythonSymbols):
+
+    def __init__(self, method, config: dict[str, Any] | None = None):
+        super().__init__(config)
+        self._method = method
+
+    @property
+    def return_value_annotation(self) -> str:
+        if self._method.return_value is None:
+            return "None"
+        if isinstance(self._method.return_value, list):
+            return self.response_class_name
+        return self._method.return_value.python.annotation
+
+    @property
+    def response_class_name(self) -> str:
+        return f"{stringmanip.upper_camel_case(self._method.name)}MethodResponse"
+
+    @property
+    def request_class_name(self) -> str:
+        return f"{stringmanip.upper_camel_case(self._method.name)}MethodRequest"
+
+
+class PythonPropertySymbols(PythonSymbols):
+
+    def __init__(self, prop, config: dict[str, Any] | None = None):
+        super().__init__(config)
+        self._prop = prop
+
+    @property
+    def class_name(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return self._prop._arg_list[0].python.class_name
+        else:
+            return f"{stringmanip.upper_camel_case(self._prop.name)}Property"
+
+    @property
+    def type(self) -> str:
+        return self.class_name
+
+    @property
+    def local_type(self) -> str:
+        return self.class_name.split(".")[-1]
+
+    @property
+    def annotation(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return self._prop._arg_list[0].python.annotation
+        else:
+            return f"{stringmanip.upper_camel_case(self._prop.name)}Property"
+
+    @property
+    def getter_value_annotation(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return self._prop._arg_list[0].python.annotation
+        return self.model_class_name
+
+    @property
+    def getter_value_annotation(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return self._prop._arg_list[0].python.annotation
+        else:
+            return self.model_class_name
+
+    @property
+    def setter_value_annotation(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return f"Union[{self._prop._arg_list[0].python.annotation}, {self.model_class_name}]"
+        else:
+            return self.model_class_name
+
+    @property
+    def model_class_name(self) -> str:
+        return f"{stringmanip.upper_camel_case(self._prop.name)}Property"
---
+++ stingeripc/rust_symb.py
@@ -0,0 +1,222 @@
+from typing import Any
+
+from jacobsjinjatoo import stringmanip
+
+from stingeripc.args import ArgPrimitiveType
+from stingeripc.lang_symb import ISymbolsProvider
+
+
+class RustSymbolsProvider(ISymbolsProvider):
+
+    def for_model(self, model_class_name: str, model) -> object | None:
+        if model_class_name == "StingerSpec":
+            return RustInterfaceSymbols(model, self.config)
+        elif model_class_name == "InterfaceEnum":
+            return RustEnumSymbols(model)
+        elif model_class_name == "InterfaceStruct":
+            return RustStructSymbols(model)
+        elif model_class_name == "Method":
+            return RustMethodSymbols(model)
+        elif model_class_name == "Property":
+            return RustPropertySymbols(model)
+        elif model_class_name == "ArgEnum":
+            return RustArgEnumSymbols(model)
+        elif model_class_name == "ArgPrimitive":
+            return RustArgPrimitiveSymbols(model)
+        elif model_class_name == "ArgStruct":
+            return RustArgStructSymbols(model)
+        elif model_class_name == "ArgDateTime":
+            return RustArgDateTimeSymbols(model)
+        elif model_class_name == "ArgDuration":
+            return RustArgDurationSymbols(model)
+        elif model_class_name == "ArgBinary":
+            return RustArgBinarySymbols(model)
+        elif model_class_name == "ArgArray":
+            return RustArgArraySymbols(model)
+        return None
+
+
+class RustSymbols:
+
+    def __init__(self, config: dict[str, Any] | None = None):
+        self.config = config
+
+
+class RustInterfaceSymbols(RustSymbols):
+
+    def __init__(self, interface, config: dict[str, Any] | None = None):
+        super().__init__(config)
+        self._iface = interface
+
+    @property
+    def package_name(self) -> str:
+        """ Name of the rust package for the interface client."""
+        s = f"{stringmanip.snake_case(self._iface.name)}_{stringmanip.snake_case(self.config.rust.package_suffix) or 'ipc'}"
+        return s
+
+    @property
+    def client_struct_name(self) -> str:
+        """ Name of the rust struct for the interface client."""
+        return f"{stringmanip.upper_camel_case(self._iface.name)}Client"
+
+    @property
+    def server_struct_name(self) -> str:
+        """ Name of the struct for the interface server."""
+        return f"{stringmanip.upper_camel_case(self._iface.name)}Server"
+
+
+class RustEnumSymbols(RustSymbols):
+
+    def __init__(self, enum):
+        super().__init__()
+        self._enum = enum
+
+    @property
+    def local_type(self) -> str:
+        return stringmanip.upper_camel_case(self._enum.name)
+
+    @property
+    def type(self) -> str:
+        return self.local_type
+
+
+class RustStructSymbols(RustSymbols):
+
+    def __init__(self, struct):
+        super().__init__()
+        self._struct = struct
+
+    @property
+    def local_type(self) -> str:
+        return stringmanip.upper_camel_case(self._struct.name)
+
+    @property
+    def type(self) -> str:
+        return self.local_type
+
+
+class RustMethodSymbols(RustSymbols):
+
+    def __init__(self, method):
+        super().__init__()
+        self._method = method
+
+    @property
+    def return_value_type(self) -> str:
+        if self._method.return_value is None:
+            return "()"
+        elif isinstance(self._method.return_value, list):
+            return stringmanip.upper_camel_case(self._method.return_value_name)
+        return self._method.return_value.rust.type
+
+
+class RustArgSymbols(RustSymbols):
+    """Base Rust symbols for Arg objects."""
+
+    def __init__(self, arg):
+        super().__init__()
+        self._arg = arg
+
+    @property
+    def type(self) -> str:
+        return self._arg.name
+
+    @property
+    def local_type(self) -> str:
+        return self.type
+
+
+class RustArgEnumSymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        if self._arg.optional:
+            return f"Option<{self._arg.enum.rust.type}>"
+        return self._arg.enum.rust.type
+
+    @property
+    def local_type(self) -> str:
+        if self._arg.optional:
+            return f"Option<{self._arg.enum.rust.local_type}>"
+        return self._arg.enum.rust.local_type
+
+
+class RustArgPrimitiveSymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        return ArgPrimitiveType.to_rust_type(self._arg.primitive_type, optional=self._arg.optional)
+
+
+class RustArgStructSymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        if self._arg.optional:
+            return f"Option<{self._arg.interface_struct.rust.type}>"
+        return self._arg.interface_struct.rust.type
+
+    @property
+    def local_type(self) -> str:
+        if self._arg.optional:
+            return f"Option<{self._arg.interface_struct.rust.local_type}>"
+        return self._arg.interface_struct.rust.local_type
+
+    @property
+    def temp_type(self) -> str:
+        return self._arg.interface_struct.rust.local_type
+
+
+class RustArgDateTimeSymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        if self._arg.optional:
+            return "Option<chrono::DateTime<chrono::Utc>>"
+        return "chrono::DateTime<chrono::Utc>"
+
+
+class RustArgDurationSymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        if self._arg.optional:
+            return "Option<chrono::Duration>"
+        return "chrono::Duration"
+
+
+class RustArgBinarySymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        if self._arg.optional:
+            return "Option<Vec<u8>>"
+        return "Vec<u8>"
+
+
+class RustArgArraySymbols(RustArgSymbols):
+
+    @property
+    def type(self) -> str:
+        if self._arg.optional:
+            return f"Option<Vec<{self._arg.element.rust.type}>>"
+        return f"Vec<{self._arg.element.rust.type}>"
+
+
+class RustPropertySymbols(RustSymbols):
+
+    def __init__(self, prop):
+        super().__init__()
+        self._prop = prop
+
+    @property
+    def local_type(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return self._prop._arg_list[0].rust.local_type
+        return f"{stringmanip.upper_camel_case(self._prop.name)}Property"
+
+    @property
+    def type(self) -> str:
+        if len(self._prop._arg_list) == 1:
+            return self._prop._arg_list[0].rust.type
+        return self.local_type
