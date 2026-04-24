913cad4b64e98fcf371021bd0101d99609162775
Jacob Brunson <jacob.brunson@vivint.com>
2026-04-24 14:24:33 -0600

    chore: bump version to 0.8.10

--- stingeripc/asyncapi.py
+++ stingeripc/asyncapi.py
@@ -31,28 +31,20 @@ class ObjectSchema:
         self._required = set()
         self._dependent_schemas = {}
 
-    def add_value_property(
-        self, name: str, arg_primitive_type: ArgPrimitiveType, required=True
-    ):
+    def add_value_property(self, name: str, arg_primitive_type: ArgPrimitiveType, required=True):
         schema = {"type": ArgPrimitiveType.to_json_type(arg_primitive_type)}
         self._properties[name] = schema
         if required:
             self._required.add(name)
 
     def add_value_dependency(self, name: str, required_on_name: str, required_on_value):
-        self._dependent_schemas[name] = {
-            "properties": {required_on_name: {"const": required_on_value}}
-        }
+        self._dependent_schemas[name] = {"properties": {required_on_name: {"const": required_on_value}}}
 
-    def add_const_value_property(
-        self, name: str, arg_type: ArgPrimitiveType, const_value, required=True
-    ):
+    def add_const_value_property(self, name: str, arg_type: ArgPrimitiveType, const_value, required=True):
         self.add_value_property(name, arg_type, required)
         self._properties[name]["const"] = const_value
 
-    def add_enum_value_property(
-        self, name: str, arg_type: ArgPrimitiveType, possible_values, required=True
-    ):
+    def add_enum_value_property(self, name: str, arg_type: ArgPrimitiveType, possible_values, required=True):
         self.add_value_property(name, arg_type, required)
         self._properties[name]["enum"] = possible_values
 
@@ -152,22 +144,16 @@ class Channel(object):
         self._operation_traits.append(trait)
         return self
 
-    def get_operation(
-        self, client_type: SpecType, use_common=False
-    ) -> dict[str, dict[str, Any]]:
+    def get_operation(self, client_type: SpecType, use_common=False) -> dict[str, dict[str, Any]]:
         channel_item: dict[str, dict[str, Any]] = dict()
         op_item: OrderedDict[str, Any] = OrderedDict(
             {
                 "operationId": self.name,
-                "message": {
-                    "$ref": f"{use_common or ''}#/components/messages/{self.message_name}"
-                },
+                "message": {"$ref": f"{use_common or ''}#/components/messages/{self.message_name}"},
             }
         )
         if use_common is not False:
-            op_item["traits"] = [
-                {"$ref": f"{use_common}#/components/operationTraits/{self.name}"}
-            ]
+            op_item["traits"] = [{"$ref": f"{use_common}#/components/operationTraits/{self.name}"}]
         elif len(self._operation_traits) > 0:
             op_item.update(
                 OrderedDict(
@@ -176,13 +162,7 @@ class Channel(object):
                     }
                 )
             )
-        if (
-            client_type == SpecType.SERVER
-            and self.direction == Direction.SERVER_PUBLISHES
-        ) or (
-            client_type == SpecType.CLIENT
-            and self.direction == Direction.SERVER_SUBSCRIBES
-        ):
+        if (client_type == SpecType.SERVER and self.direction == Direction.SERVER_PUBLISHES) or (client_type == SpecType.CLIENT and self.direction == Direction.SERVER_SUBSCRIBES):
             channel_item.update({"publish": op_item})
         else:
             channel_item.update({"subscribe": op_item})
@@ -241,9 +221,7 @@ class Server(object):
         if self._host is None or self._port is None:
             spec["variables"] = {}
         if self._host is None:
-            spec["variables"]["hostname"] = {
-                "description": "The hosthame or IP address of the MQTT broker."
-            }
+            spec["variables"]["hostname"] = {"description": "The hosthame or IP address of the MQTT broker."}
         if self._port is None:
             spec["variables"]["port"] = {"description": "The port for the MQTT server"}
         return spec
@@ -413,9 +391,7 @@ class AsyncApiCreator(object):
         for svr in self.servers:
             spec["servers"][svr.name] = svr.get_server()
         for ch in self.channels:
-            spec["channels"][ch.topic] = ch.get_operation(
-                client_type, use_common or False
-            )
+            spec["channels"][ch.topic] = ch.get_operation(client_type, use_common or False)
         if use_common is None:
             for msg in self.messages:
                 spec["components"]["messages"][msg.name] = msg.get_message()
@@ -438,9 +414,7 @@ class StingerToAsyncApi:
         self._add_methods()
         if len(self._stinger.methods) > 0:
             schema_name = f"stinger_method_return_codes"
-            description = [
-                f"The stinger_method_return_codes enum has the following values:"
-            ]
+            description = [f"The stinger_method_return_codes enum has the following values:"]
             accepted_values = []
             for i, enum_value in self._stinger.method_return_codes.items():
                 description.append(f"{i} - {enum_value}")
@@ -470,7 +444,7 @@ class StingerToAsyncApi:
 
     def _add_servers(self):
         info_topic = self._stinger.interface_info_topic()
-        if hasattr(self._stinger, 'brokers'):
+        if hasattr(self._stinger, "brokers"):
             for broker_name, broker_spec in self._stinger.brokers.items():
                 svr = Server(broker_name)
                 if broker_spec.hostname is not None and broker_spec.port is not None:
@@ -478,8 +452,7 @@ class StingerToAsyncApi:
                 svr.set_lwt_topic(info_topic)
                 self._asyncapi.add_server(svr)
 
-    def _add_enums(self):
-        ...
+    def _add_enums(self): ...
 
     def _add_signals(self):
         for sig_name, sig_spec in self._stinger.signals.items():
@@ -493,33 +466,23 @@ class StingerToAsyncApi:
                 if isinstance(arg_spec, ArgPrimitive):
                     schema.add_value_property(arg_spec.name, arg_spec.type)
                 elif isinstance(arg_spec, ArgEnum):
-                    schema.add_reference_property(
-                        arg_spec.name, f"#/components/schemas/enum_{arg_spec.enum.name}"
-                    )
+                    schema.add_reference_property(arg_spec.name, f"#/components/schemas/enum_{arg_spec.enum.name}")
             msg.set_schema(schema.to_schema())
             self._asyncapi.add_message(msg)
 
     def _add_methods(self):
         for method_name, method_spec in self._stinger.methods.items():
-            call_ch = Channel(
-                method_spec.request_topic(), method_name, Direction.SERVER_SUBSCRIBES
-            )
-            call_ch.add_operation_trait(
-                {"$ref": "#/components/operationTraits/methodCall"}
-            )
+            call_ch = Channel(method_spec.request_topic(), method_name, Direction.SERVER_SUBSCRIBES)
+            call_ch.add_operation_trait({"$ref": "#/components/operationTraits/methodCall"})
             self._asyncapi.add_channel(call_ch)
             call_msg = Message(method_name)
-            call_msg.add_trait(
-                {"$ref": "#/components/messageTraits/methodJsonArguments"}
-            )
+            call_msg.add_trait({"$ref": "#/components/messageTraits/methodJsonArguments"})
             call_msg_schema = ObjectSchema()
             for arg_spec in method_spec.arg_list:
                 if isinstance(arg_spec, ArgPrimitive):
                     call_msg_schema.add_value_property(arg_spec.name, arg_spec.type)
                 elif isinstance(arg_spec, ArgEnum):
-                    call_msg_schema.add_reference_property(
-                        arg_spec.name, f"#/components/schemas/enum_{arg_spec.name}"
-                    )
+                    call_msg_schema.add_reference_property(arg_spec.name, f"#/components/schemas/enum_{arg_spec.name}")
             call_msg.set_schema(call_msg_schema.to_schema())
             self._asyncapi.add_message(call_msg)
 
@@ -528,14 +491,10 @@ class StingerToAsyncApi:
                 f"{method_name}Response",
                 Direction.SERVER_PUBLISHES,
             )
-            resp_ch.add_operation_trait(
-                {"$ref": "#/components/operationTraits/methodCall"}
-            )
+            resp_ch.add_operation_trait({"$ref": "#/components/operationTraits/methodCall"})
             self._asyncapi.add_channel(resp_ch)
             resp_msg = Message(f"{method_name}Response")
-            resp_msg.add_trait(
-                {"$ref": "#/components/messageTraits/methodJsonArguments"}
-            )
+            resp_msg.add_trait({"$ref": "#/components/messageTraits/methodJsonArguments"})
             resp_msg.add_header(
                 "result",
                 {"$ref": "#/components/schemas/stinger_method_return_codes"},
@@ -546,9 +505,7 @@ class StingerToAsyncApi:
 
             def add_arg(arg: Arg):
                 if isinstance(arg, ArgPrimitive):
-                    resp_msg_schema.add_value_property(
-                        arg.name, arg.type, required=True
-                    )
+                    resp_msg_schema.add_value_property(arg.name, arg.type, required=True)
                 elif isinstance(arg, ArgEnum):
                     resp_msg_schema.add_reference_property(
                         arg.name,
@@ -561,13 +518,9 @@ class StingerToAsyncApi:
                     add_arg(arg_spec)
                     resp_msg_schema.add_value_dependency(arg_spec.name, "result", 0)
             elif method_spec.return_value is not None:
-                assert isinstance(
-                    method_spec.return_value, Arg
-                ), "Method return value must be a primitive or enum"
+                assert isinstance(method_spec.return_value, Arg), "Method return value must be a primitive or enum"
                 add_arg(method_spec.return_value)
-                resp_msg_schema.add_value_dependency(
-                    method_spec.return_value_name, "result", 0
-                )
+                resp_msg_schema.add_value_dependency(method_spec.return_value_name, "result", 0)
 
             resp_msg.set_schema(resp_msg_schema.to_schema())
             self._asyncapi.add_message(resp_msg)
