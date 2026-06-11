9f68459dc8f968a27f7a020599f6bd150ca808ba
Jacob Brunson <jacob.brunson@vivint.com>
2026-06-11 11:40:28 -0600

    Improve JS

--- stingeripc/tools/generic_generator.py
+++ stingeripc/tools/generic_generator.py
@@ -74,6 +74,7 @@ def main(
             "topic_template_placeholder_index": topic_util.get_argument_position,
             "get_topic_arguments": topic_util.get_topic_arguments,
             "template_topic_fill_in": topic_util.topic_template_fill_in,
+            "topic_template_to_js": topic_util.topic_template_to_js,
         },
     }
 
